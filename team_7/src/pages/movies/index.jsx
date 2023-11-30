dc9dd077a12d09a49f9c7c76aa626de7a40056b2
Hassan AbuGareeb <Hassan.AbuGareeb@gmail.com>
2023-11-30 10:37:31 +0300

    created a working movie page without the pagination

---
+++ team_7/src/pages/movies/index.jsx
@@ -0,0 +1,82 @@
+import styles from "@/styles/Home.module.css"
+import Link from "next/link"
+
+export default function Home({ latestMovies, filterBy }) {
+  const movies = latestMovies.map((movie, index) => {
+    return (
+      <div
+        key={index}
+        style={{
+          backgroundColor: "#999",
+          width: "150px",
+          margin: "10px auto",
+          padding: "2px",
+        }}
+      >
+        <li
+          style={{
+            margin: "10px",
+          }}
+        >
+          <Link href={`./movies/${movie.id}`}>
+            <img
+              width={"100px"}
+              src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+            />
+          </Link>
+          <br />
+          {movie.title}
+        </li>
+      </div>
+    )
+  })
+  return (
+    <div style={{ textAlign: "center" }}>
+      {filterBy
+        .split("_")
+        .map((movie) => movie.at(0).toUpperCase() + movie.slice(1))
+        .join(" ")}
+      <div style={{ margin: "30px auto" }}>
+        <ul
+          style={{
+            margin: "10px",
+            listStyle: "none",
+            padding: "0",
+            margin: "10px auto",
+            display: "flex",
+            flexWrap: "wrap",
+          }}
+        >
+          {"loading..." && movies}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export async function getServerSideProps({ query }) {
+  const filterBy = query.filter
+  //fetch options
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMjcxYTRhY2NkMGUwY2I0NzBmYWZkMjlhMmJjOTZjNiIsInN1YiI6IjY1NjYwODU3YTM0OTExMDExYjU5MTk2YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Bd7zqZOCEOyovLHdwMIyHB6BX_EgPzxw6JCCTiLNriQ",
+    },
+  }
+
+  //get the latest movies
+  const latestMoviesResp = await fetch(
+    `https://api.themoviedb.org/3/movie/${filterBy}?language=en-US&page=${query.page}`,
+    options,
+  )
+  const data = await latestMoviesResp.json()
+  const latestMovies = [...data.results]
+  return {
+    props: {
+      latestMovies,
+      filterBy,
+    },
+  }
+}
